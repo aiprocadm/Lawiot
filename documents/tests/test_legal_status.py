1ed9cac63386b46bbfd61406883e9e59a5610f9c
aiproc.adm <aiproc.adm@gmail.com>
2026-06-23 23:22:54 +0300

    test(documents): использовать фабрики make_document/make_redaction (ревью)

--- documents/tests/test_legal_status.py
+++ documents/tests/test_legal_status.py
@@ -1,13 +1,12 @@
-import datetime
-
 import pytest
 
 from documents.models import Document, Redaction
+from documents.tests.factories import make_document, make_redaction
 
 
 @pytest.mark.django_db
 def test_new_document_defaults_to_federal_official():
-    doc = Document.objects.create(slug="d1", doc_type="code", title="Акт")
+    doc = make_document(slug="d1")
     assert doc.level == Document.Level.FEDERAL
     assert doc.source_status == Document.SourceStatus.OFFICIAL
     assert doc.region_code == ""
@@ -15,7 +14,6 @@ def test_new_document_defaults_to_federal_official():
 
 @pytest.mark.django_db
 def test_new_redaction_defaults_to_official_text():
-    doc = Document.objects.create(slug="d2", doc_type="code", title="Акт")
-    red = Redaction.objects.create(document=doc, redaction_date=datetime.date(2020, 1, 1))
+    red = make_redaction(make_document(slug="d2"))
     assert red.text_status == Redaction.TextStatus.OFFICIAL
     assert red.get_text_status_display() == "Официальная редакция"
